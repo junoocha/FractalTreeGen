81c64165a0c89e00079185aaa99182d83aef9d49
[Caleb Cha] <junoocha@gmail.com>
2025-07-03 20:53:45 -0400

    removed count, external import

--- src/components/tree-controls.tsx
+++ src/components/tree-controls.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import TreeWindow from './tree-window';
-import { count } from 'console';
 
 function calculateTotalBranches(
   branchesPerLevel: number,
